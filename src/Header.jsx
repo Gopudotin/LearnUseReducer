e8b326bd191d010cb4e4108f2a47f1b5260050f2
Gopas-coder <53084994+Gopas-coder@users.noreply.github.com>
2024-02-20 17:44:48 +0530

    use reducer pt 3

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,10 @@
+import React from "react";
+import "./logo.png";
+
 function Header() {
   return (
     <header className='app-header'>
-      <img src='logo512.png' alt='React logo' />
+      <img src='logo.png' alt='React logo' />
       <h1>The React Quiz</h1>
     </header>
   );
